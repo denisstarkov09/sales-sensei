4dfaee7c3e4cdd9428712ab3c5118fed260ef43e
hgtiger <hgtiger@gmail.com>
2023-08-30 01:40:35 -0700

    go home when click a logo of main page.

--- src/layouts/main/sidebar.jsx
+++ src/layouts/main/sidebar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-[280px] shrink-0 bg-[#1C2536] h-[100vh] relative">
-      <div className="w-[100%] h-[100px] dropped1 flex items-center px-[30px] drop-shadow-xl  relative">
-        <img src="/bgLogo.svg" alt="" />
-      </div>
+      <Link to={"/"}>
+        <div className="w-[100%] h-[100px] dropped1 flex items-center px-[30px] drop-shadow-xl  relative">
+          <img src="/bgLogo.svg" alt="" />
+        </div>
+      </Link>
       {/* <div className="w-[100%] px-[20px] h-[100px] flex items-center justify-center">
         <div className="w-[100%] h-[60px] rounded-[22px] px-[20px] bg-[#232C3C] flex items-center">
           <img src="/plus.svg" className="w-[20px] mr-[15px]" alt="" />
